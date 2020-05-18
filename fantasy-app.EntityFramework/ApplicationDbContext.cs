52fd97e81328db99991cfb58e30c6fbb6cf0f269
Luka Radovanovic <lukadlm97@gmail.com>
2020-05-19 00:30:51 +0200

    updated .gitignore file and setup project with indentity migration

---
+++ fantasy-app.EntityFramework/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace fantasy_app.EntityFramework
+{
+    public class ApplicationDbContext:IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            :base(options)
+        {
+        }
+
+
+    }
+}
---
+++ fantasy-app.EntityFramework/DesignTimeDbContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace fantasy_app.EntityFramework
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>();
+            var connectionString = @"Server = (localdb)\MSSQLLocalDB; Database = fantasy_app_Dev; Trusted_Connection = True; MultipleActiveResultSets = true";
+
+            options.UseSqlServer(connectionString);
+
+            return new ApplicationDbContext(options.Options);
+        }
+    }
+}
